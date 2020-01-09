b771f0595f3442035f62f4978c9ac72802a32217
Massil TAGUEMOUT <massitaguemout@gmail.com>
2020-01-09 17:24:43 +0100

    update block.java

--- src/Block.java
+++ src/Block.java
@@ -1,19 +1,112 @@
+import java.time.Instant;
 
 public class Block {
-    public String previous_hash;
+    static int nbBlocks = 0;
+    private String previousHash;
 
-    public String hash;
+    private String hash;
 
-    public int index;
+    private Transaction data;
 
-    public long nonce;
+    private int index;
 
-    public long timestamp;
+    private int nonce;
 
-    public String hashCode() {
+    private long timestamp;
+
+    public Block(String previousHash, Transaction data, long timestamp) {
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+        this.nonce = 0;
+        this.index = nbBlocks;
+        nbBlocks++;
+        this.hash = hash();
     }
 
-    public Block newBlock(Transaction transaction, String previous_hash) {
+    public String hash() {
+        String input = previousHash
+                + data.toString()
+                + timestamp
+                + nonce;
+        return StringUtil.SHA256(input);
     }
 
+    public static Block genesisBlock(Transaction data){
+        return newBlock(StringUtil.zeros(64),data);
+    }
+
+    // TODO add ProofOfWork + setHash + setNonce
+    public static Block newBlock(String previousHash, Transaction transaction) {
+        Block block = new Block(previousHash, transaction, Instant.now().getEpochSecond());
+        // HERE
+        return block;
+    }
+
+    public String mine(int prefix){
+        String prefixString = new String(new char[prefix]).replace('\0', '0');
+        while (!hash.substring(0, prefix).equals(prefixString)) {
+            nonce++;
+            hash = hash();
+        }
+        return hash;
+    }
+
+
+    // Getters and Setters
+    public static int getNbBlocks() {
+        return nbBlocks;
+    }
+
+    public static void setNbBlocks(int nbBlocks) {
+        Block.nbBlocks = nbBlocks;
+    }
+
+    public String getPreviousHash() {
+        return previousHash;
+    }
+
+    public void setPreviousHash(String previousHash) {
+        this.previousHash = previousHash;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public Transaction getData() {
+        return data;
+    }
+
+    public void setData(Transaction data) {
+        this.data = data;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public int getNonce() {
+        return nonce;
+    }
+
+    public void setNonce(int nonce) {
+        this.nonce = nonce;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
 }
