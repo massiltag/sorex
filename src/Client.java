73c076e58dd449fca087d883d68bb08328c50459
Gabriel <gab00mail@protonmail.com>
2020-01-13 21:22:56 +0100

    client + server

--- src/Client.java
+++ src/Client.java
@@ -1,45 +1,60 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
 
 public class Client {
-    private int id;
+	private int id;
+	private String email;
+	private String name;
+	private Server server;
+	private Socket socket;
+	private Scanner scanner;
 
-    private String email;
-
-    private String name;
-
-    private Server server;
-
-    public void effectuerTransaction(int montant, int id) {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Server getServer() {
-        return server;
-    }
-
-    public void setServer(Server server) {
-        this.server = server;
-    }
+	private Client(InetAddress serverAddress, int serverPort) throws Exception {
+		this.socket = new Socket(serverAddress, serverPort);
+		this.scanner = new Scanner(System.in);
+	}
+	private void start() throws IOException {
+		String input;
+		while (true) {
+			input = scanner.nextLine();
+			PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
+			out.println(input);
+			out.flush();
+		}
+	}
+	public void effectuerTransaction(int montant, int id) {
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Server getServer() {
+		return server;
+	}
+	public void setServer(Server server) {
+		this.server = server;
+	}
+	public static void main(String[] args) throws Exception {
+		Client client = new Client(
+				InetAddress.getByName(args[0]),
+				Integer.parseInt(args[1])
+				);
+		System.out.println("\r\nConnected to server: " + client.socket.getInetAddress());
+		client.start();
+	}
 }
--- src/Server.java
+++ src/Server.java
@@ -1,9 +1,55 @@
+import java.net.*;
+import java.io.*;
 
 public class Server {
-    public Blockchain blockchain;
+	public Blockchain blockchain;
+	private ServerSocket server;
 
-    public boolean TCPConnect() {
-        return false;
-    }
+	public Server(String ipAddress) throws Exception {
+		if (ipAddress != null && !ipAddress.isEmpty()) {
+			this.server = new ServerSocket(
+					0, 1, InetAddress.getByName(ipAddress)
+					);
+		} else {
+			this.server = new ServerSocket(
+					0, 1, InetAddress.getLocalHost()
+					);
+		}
+	}
 
+	private void listen() throws Exception {
+		String data = null;
+		Socket client = this.server.accept();
+		String clientAddress = client.getInetAddress().getHostAddress();
+		System.out.println("\r\nNew connection from : " + clientAddress);
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(
+					client.getInputStream()
+					)
+				);
+		while ((data = in.readLine()) != null) {
+			System.out.println("\r\nMessage from : " + clientAddress +
+					" : " + data);
+		}
+	}
+
+	public InetAddress getSocketAddress() {
+		return this.server.getInetAddress();
+	}
+
+	public int getPort() {
+		return this.server.getLocalPort();
+	}
+
+	public boolean TCPConnect() {
+		return false;
+	}
+	public static void main(String[] args) throws Exception {
+		Server app = new Server(args[0]);
+		System.out.println("\r\nRunning server : " +
+				" Host=" + app.getSocketAddress().getHostAddress() +
+				" Port=" + app.getPort()
+				);
+		app.listen();
+	}
 }
