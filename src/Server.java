59f883db70df722baf69ca761c781989334b1eba
Massil TAGUEMOUT <massitaguemout@gmail.com>
2020-01-14 18:17:52 +0100

    fix TCP connection

--- src/Server.java
+++ src/Server.java
@@ -1,55 +1,104 @@
 import java.net.*;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
-	public Blockchain blockchain;
-	private ServerSocket server;
+	private final static int port = 3025;
 
-	public Server(String ipAddress) throws Exception {
-		if (ipAddress != null && !ipAddress.isEmpty()) {
-			this.server = new ServerSocket(
-					0, 1, InetAddress.getByName(ipAddress)
-					);
-		} else {
-			this.server = new ServerSocket(
-					0, 1, InetAddress.getLocalHost()
-					);
-		}
-	}
+	public static Blockchain blockchain = Blockchain.getInstance(2);
 
-	private void listen() throws Exception {
-		String data = null;
-		Socket client = this.server.accept();
-		String clientAddress = client.getInetAddress().getHostAddress();
-		System.out.println("\r\nNew connection from : " + clientAddress);
-		BufferedReader in = new BufferedReader(
-				new InputStreamReader(
-					client.getInputStream()
-					)
-				);
-		while ((data = in.readLine()) != null) {
-			System.out.println("\r\nMessage from : " + clientAddress +
-					" : " + data);
-		}
-	}
+	private static ServerSocket serverSocket;
 
-	public InetAddress getSocketAddress() {
-		return this.server.getInetAddress();
-	}
+	private static List<Socket> connectedClients = new ArrayList<>();
 
-	public int getPort() {
-		return this.server.getLocalPort();
-	}
-
-	public boolean TCPConnect() {
-		return false;
-	}
 	public static void main(String[] args) throws Exception {
-		Server app = new Server(args[0]);
-		System.out.println("\r\nRunning server : " +
-				" Host=" + app.getSocketAddress().getHostAddress() +
-				" Port=" + app.getPort()
-				);
-		app.listen();
+		initSocket();
+
+		// Starting Threads
+		connectionListener();
+	}
+
+	private static void initSocket() {
+		try {
+			serverSocket = new ServerSocket(port);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Server running on port " + port);
+	}
+
+	private static void connectionListener() {
+		Runnable task = () -> {
+			while (true) {
+				Socket socket;
+				try {
+					socket = acceptConnect(serverSocket);
+					connectedClients.add(socket);
+					receiveAndSendTransaction(socket);
+				} catch (Exception e) {
+					System.err.println("Connection Error (34)");
+				}
+			}
+		};
+		new Thread(task).start();
+	}
+
+	private static Socket acceptConnect(ServerSocket serverSocket) throws Exception {
+		Socket clientSocket = serverSocket.accept();
+		System.out.println("[+] New connection accepted from " + getSocketIP(clientSocket));
+		return clientSocket;
+	}
+
+	private static void receiveAndSendTransaction(Socket socket) {
+		Runnable task = () -> {
+			while (true) {
+				// Receiving a transaction request
+				Transaction transaction = null;
+				try {
+					ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+					transaction = (Transaction) ois.readObject();
+					System.out.println("[+] Received new transaction request : [" + transaction.getValue() + " SorEx] from " + getSocketIP(socket) + " to " + transaction.getReceiverIP() + ".");
+				} catch (IOException | ClassNotFoundException e) {
+					System.err.println("Error (58)"); // TODO DEL
+				}
+
+				// Forwarding the transaction to the receiver
+				try {
+					Socket receiver = null;
+					for (Socket s : connectedClients) {
+						if (s.getInetAddress().getHostAddress().equals(transaction.getReceiverIP()))
+							receiver = s;
+					}
+					if (receiver == null || receiver.isClosed()) throw new UnknownHostException();
+
+					ObjectOutputStream receiver_oos = new ObjectOutputStream(receiver.getOutputStream());
+					receiver_oos.writeObject(transaction);
+
+					reply(socket, transaction, "ACK");
+					System.out.print("[+] " + transaction.getValue() + " SorEx coins transferred from " + getSocketIP(socket) + " to " + getSocketIP(receiver) + ".");
+
+					blockchain.addBlock(transaction); // TODO CHECK THIS
+				} catch (UnknownHostException e) {
+					reply(socket, transaction, "ERR");
+					System.out.print("[-] Error while processing transaction from " + getSocketIP(socket) + " to " + transaction.getReceiverIP() + "\n[ ] Unknown host.");
+				} catch (Exception e) {
+				}
+			}
+		};
+		new Thread(task).start();
+	}
+
+	public static void reply(Socket socket, Transaction transaction, String status) {
+		try {
+			ObjectOutputStream sender_oos = new ObjectOutputStream(socket.getOutputStream());
+			transaction.setControlValue(status);
+			sender_oos.writeObject(transaction);
+		} catch (IOException e) {
+		}
+	}
+
+	public static String getSocketIP(Socket socket) {
+		return socket.getInetAddress().getHostAddress();
 	}
 }
